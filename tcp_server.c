20db8660e3afb4ea6010420aa8d69074e668e326
bil74 <pbil@freemail.hu>
2022-03-21 14:30:27 +0100

    try

--- tcp_server.c
+++ tcp_server.c
@@ -7,6 +7,7 @@
 #include <ws2tcpip.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <conio.h>
 
 // Need to link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
@@ -41,7 +42,9 @@ int main(int argc, char *argv[])
     char recvbuf[DEFAULT_BUFLEN];
     int recvbuflen = DEFAULT_BUFLEN;
 	int wasParamError = 0;
-    
+
+	FD_SET ReadSet;
+
 	//get params
 	if (argc > 1){
 		int i;
@@ -91,10 +94,28 @@ int main(int argc, char *argv[])
 		char portStr[10];
 		sprintf(portStr, "%d", port);
 		iResult = getaddrinfo(NULL, portStr, &hints, &result);
+		//(getaddrinfo output parameter is a linked list as result, which can have more results. Try until connect is successful!)
 		if ( iResult != 0 ) {
 			printf("getaddrinfo failed with error: %d\n", iResult);
 			WSACleanup();
 			return 1;
+		}
+	}
+
+	//testing result
+	{
+		struct addrinfo *next = result;
+		while(next){
+			printf("%s\n","(***test) getaddrinfo result check");
+			printf("(***test) ai_flags:%d\n", next->ai_flags);
+			printf("(***test) ai_family:%d\n", next->ai_family);
+			printf("(***test) ai_socktype:%d\n", next->ai_socktype);
+			printf("(***test) ai_protocol:%d\n", next->ai_protocol);
+			printf("(***test) ai_addrlen:%d\n", next->ai_addrlen);
+			printf("(***test) ai_canonname:%s\n", next->ai_canonname);
+			printf("(***test) ai_addr->sa_family:%d\n", next->ai_addr->sa_family);
+			printf("(***test) ai_addr->sa_data:%s\n", next->ai_addr->sa_data);
+			next = next->ai_next;
 		}
 	}
 
@@ -127,47 +148,74 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Accept a client socket
-    ClientSocket = accept(ListenSocket, NULL, NULL);	//waits here
-    if (ClientSocket == INVALID_SOCKET) {
-        printf("accept failed with error: %d\n", WSAGetLastError());
-        closesocket(ListenSocket);
-        WSACleanup();
-        return 1;
-    }
+	//set ListenSocket to blocking only 1 millisecond
+	{
+		struct timeval tv;
+		tv.tv_sec = 1;
+		tv.tv_usec = 0;	//1000
+		FD_ZERO(&ReadSet);
+		FD_SET(ListenSocket, &ReadSet);
+		iResult = select(0, &ReadSet, NULL, NULL, &tv);
+	}
 
-    // No longer need server socket
-    closesocket(ListenSocket);
+	printf("entering cycle\n");
 
     // Receive until the peer shuts down the connection
+	fflush(stdin);
     do {
-
-        iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
-        if (iResult > 0) {
-            printf("Bytes received: %d\n", iResult);
-
-			if (mode == m_mirror){
-				// Echo the buffer back to the sender
-				iSendResult = send( ClientSocket, recvbuf, iResult, 0 );
-				if (iSendResult == SOCKET_ERROR) {
-					printf("send failed with error: %d\n", WSAGetLastError());
-					closesocket(ClientSocket);
-					WSACleanup();
-					return 1;
-				}
-				printf("Bytes sent: %d\n", iSendResult);
+		printf("wait 1 sec...\n");
+		Sleep(1000);
+		if (FD_ISSET(ListenSocket, &ReadSet)){
+			printf("FD_ISSET\n");
+			// Accept a client socket
+			ClientSocket = accept(ListenSocket, NULL, NULL);	//waits here 1 milliseconds
+			if (ClientSocket == INVALID_SOCKET) {
+				printf("accept failed with error: %d\n", WSAGetLastError());
+				closesocket(ListenSocket);
+				WSACleanup();
+				return 1;
 			}
-        }
-        else if (iResult == 0)
-            printf("Connection closing...\n");
-        else  {
-            printf("recv failed with error: %d\n", WSAGetLastError());
-            closesocket(ClientSocket);
-            WSACleanup();
-            return 1;
-        }
 
-    } while (iResult > 0);
+			iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+			if (iResult > 0) {
+				printf("Bytes received: %d\n", iResult);
+
+				if (mode == m_mirror){
+					// Echo the buffer back to the sender
+					iSendResult = send( ClientSocket, recvbuf, iResult, 0 );
+					if (iSendResult == SOCKET_ERROR) {
+						printf("send failed with error: %d\n", WSAGetLastError());
+						closesocket(ClientSocket);
+						WSACleanup();
+						return 1;
+					}
+					printf("Bytes sent: %d\n", iSendResult);
+				}
+			}
+			else if (iResult == 0)
+				printf("Connection closing...\n");
+			else  {
+				printf("recv failed with error: %d\n", WSAGetLastError());
+				closesocket(ClientSocket);
+				WSACleanup();
+				return 1;
+			}
+
+			// shutdown the connection since we're done
+			iResult = shutdown(ClientSocket, SD_SEND);
+			if (iResult == SOCKET_ERROR) {
+				printf("shutdown failed with error: %d\n", WSAGetLastError());
+				closesocket(ClientSocket);
+				WSACleanup();
+				return 1;
+			}
+			//close client socket
+			closesocket(ClientSocket);
+
+			
+		}
+
+    } while ( !(_kbhit() && _getch() == 27) /*iResult > 0*/);
 
     // shutdown the connection since we're done
     iResult = shutdown(ClientSocket, SD_SEND);
@@ -179,8 +227,9 @@ int main(int argc, char *argv[])
     }
 
     // cleanup
-    closesocket(ClientSocket);
-    WSACleanup();
+	closesocket(ClientSocket);
+    closesocket(ListenSocket);
+	WSACleanup();
 
     return 0;
 
